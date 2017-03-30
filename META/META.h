d924cbf63e851c88b7c94d8e08568dbb77aabbb4
pixelskull <pascal.schoenthier@th-koeln.de>
2017-03-30 15:11:31 +0200

    Initial Commit

---
+++ META/META.h
@@ -0,0 +1,19 @@
+//
+//  META.h
+//  META
+//
+//  Created by Pascal Schönthier on 30.03.17.
+//  Copyright © 2017 Pascal Schönthier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for META.
+FOUNDATION_EXPORT double METAVersionNumber;
+
+//! Project version string for META.
+FOUNDATION_EXPORT const unsigned char METAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <META/PublicHeader.h>
+
+
